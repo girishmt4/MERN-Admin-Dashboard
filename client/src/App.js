f709f253040ce7648a0f41da53715bb4dad2ef33
Girish Tiwale <girishmt4@gmail.com>
2023-03-01 18:01:40 -0800

    Basic Project Setup

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Layout from "./components/Layout";
+import { themeSettings } from "./theme";
+
+
+function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />} >
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+
+const Layout = () => {
+  return (
+    <Box width="100%" height="100%">
+      <Box>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
